785d8095882a4aa9cb34418e35c089a86a036cb7
Moraknik <7013333a@gmail.com>
2024-07-09 15:25:25 +0300

    Update t7.go

--- GO_HW_1/t7.go
+++ GO_HW_1/t7.go
@@ -1,18 +1,25 @@
 package main
-package primes
 
-import (
-	"fmt"
-	"primes"
-)
+import "fmt"
+
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
 
 func main() {
 	var n int
 	fmt.Scanln(&n)
-	for i = 1; i <= n; ++i {
-		if IsPrime(i) {
-			fmt.Print(i)
-			}
+	for i := 1; i <= n; i++ {
+		if isPrime(i) {
+			fmt.Println(i)
 		}
 	}
 }
